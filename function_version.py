b5286f76a00fd940debfa3a1eb8117c661e0df70
gkrait <gmatharab1991@gmail.com>
2020-03-19 02:44:22 +0100

    old version

--- function_version.py
+++ function_version.py
@@ -15,6 +15,10 @@ from pprint import pprint
 
 def compose(f, g):
     return lambda x: f(g(x))
+def composenbox(func,g):  # input of f is a list of n ft.arb and g is a list of 2n-1 ft.arb
+  return lambda U:[funci(g(U)) for funci in func ]
+
+
 def sqrt_t(I):
   if 0 in I:
     return d.ftconstructor(0,math.sqrt(float(I.upper())))
@@ -38,9 +42,10 @@ def F_Ballminus(U):    # len(U) is odd
   return F2          
 
 def Ball_func(func,Jac_func): # func is a function that sends a list of intervals to an internal ....   Jac_func(i) is the pratial dervative of func wrt the i-variable 
-   S_func=lambda U: 1/2*(compose(func,F_Ballplus)(U)+compose(func,F_Ballplus)(U))
-   D_func= lambda U: 1/(2*sqrt_t(U[len(U)-1]))*(compose(func,F_Ballplus)(U)+compose(func,F_Ballplus)(U)) if 0 not \
-    in U[len(U)-1] else sum([d.intervals_multi(nabla_funci(U),ri) for Yi,ri in zip(Jac_func,[0,0]+U[int((len(U)+1)/2):len(U)-1])  ])
+
+   S_func=lambda U: 1/2*(compose(func,F_Ballplus)(U)+compose(func,F_Ballminus)(U))
+   D_func= lambda U: 1/(2*sqrt_t(U[len(U)-1]))*(compose(func,F_Ballplus)(U)+compose(func,F_Ballminus)(U)) if 0 not \
+    in U[len(U)-1] else sum([d.intervals_multi(nabla_funci(U),ri) for nabla_funci,ri in zip(Jac_func,[0,0]+U[int((len(U)+1)/2):len(U)-1])  ])
    return [S_func,D_func] 
 
 
@@ -49,6 +54,21 @@ def poly_list_tofunc(P):
 def eval_func(func,U):
   return func(U)
 
+
+def complete_jac_ball(func,jac_func,H_func,U): 
+  jac_ball=[]
+  SP=[]
+  DP=[]
+  for i in range(len(func)):
+     
+
+     SP.append(jac_Ball_of_onefunction(func[i],jac_func[i],H_func[i],U)[0] )
+     DP.append(jac_Ball_of_onefunction(func[i],jac_func[i],H_func[i],U)[1] )
+  
+  jac_ball=SP+DP
+  last_eq= lambda U:  [0]*int((len(U)+1)/2) + U[ int((len(U)+1)/2): len(U)-1  ] +[0] 
+  jac_ball.append(last_eq(U))
+  return jac_ball
 
 def invertability_jac_Ball(func,Jac_func,U): #still not ready 
   if 0 not in U[len(U)-1]:
@@ -70,10 +90,10 @@ def invertability_jac_Ball(func,Jac_func,U): #still not ready
 def jac_Ball_of_onefunction(func,Jac_func,H_func,U): #func is from R^n to R.. Jac_func is the Jacobian of func 
   n=int((len(U)+1)/2)
   answer=[]
-
   #Computing the partial derivatives of S.func (see the proof of Lemma 55)
   for j in range(n):
-    answer.append(Ball_func(Jac_func[j],H_func[j])[0](U))
+    f=Ball_func(Jac_func[j],H_func[j])[0]
+    answer.append(f(U))
   for j in range(2,n):
     answer.append(d.intervals_multi(Ball_func(Jac_func[j],H_func[j])[1](U), U[2*n-2]))
     D_P=[Ball_func(Jac_func[j],H_func[j])[1](U) for j in range(2,n)]
@@ -82,7 +102,8 @@ def jac_Ball_of_onefunction(func,Jac_func,H_func,U): #func is from R^n to R.. Ja
   for i in range(2,n):
     sum += d.intervals_multi(D_P[i-2],U[i+n-2])
   answer.append(sum) 
-
+  SP=answer
+  answer=[]
   ############################################""
   #Computing the partial derivatives of D.func 
   for j in range(n):
@@ -101,28 +122,27 @@ def jac_Ball_of_onefunction(func,Jac_func,H_func,U): #func is from R^n to R.. Ja
     answer.append(DP_t)
   else:
         pass
-    
-  
-  
 
-
-
-
-  return answer    
+  return [SP,answer]    
 
 
 def func_matrixval(jac,X): #jac as i_minor and X is a list of ft.arb
-         evaluation_jac_X=[copy(jaci) for jaci in jac]
-         for i in range(len(jac)):
-             for j in range(len(jac[0])):
-                 #print(evaluation_jac_X[i][j])
-                 #input()
-                 evaluation_jac_X[i][j]=jac[i][j](X)
-                 if evaluation_jac_X[i][j]==0:       #fixing a bug in flint since Python cannot create arb from type <class 'sympy.core.numbers.Zero'>
-                     evaluation_jac_X[i][j]=ft.arb(0)
-         return evaluation_jac_X
+         #for i in range(len(jac)):
+          #   for j in range(len(jac[0])):
+
+          #       evaluation_jac_X[i][j]=jac[i][j](X)
+          #       if evaluation_jac_X[i][j]==0:       #fixing a bug in flint since Python cannot create arb from type <class 'sympy.core.numbers.Zero'>
+          #           evaluation_jac_X[i][j]=ft.arb(0)
+         
+          T=[]
+          for i in range(len(jac)):
+             T.append([])
+             for j in range(len(jac[i])):
+              T[i].append(jac[i][j](X))
+          return T
 
 def func_solver(P,jac,B,k=2): #k is the number of parts in which every interval is divided  
+
     it=0
     Solutions=[]
     L=[B]
@@ -146,23 +166,14 @@ def func_solver(P,jac,B,k=2): #k is the number of parts in which every interval 
                    jac_eval_current_box=func_matrixval(jac,current_box)
                    mid_box=[ft.arb(float(interval.mid()))  for interval in current_box]
                    b=[Pi(mid_box) for Pi in P]
-                   """print('mid',mid_box)
-                   print('the invertability',invertibility_of_a_matrix(jac_eval_current_box))
-                   try:
-                    pprint(jac_eval_current_box)
-                   except:
-                    pass
-                   print('b',b)
-                   ftprint(current_box)
-                   input()"""
+                   print(jac[0][0](current_box))
+                   d.ftprint(current_box)
+                   d.ftprint(value_of_P_current_box)
+                   pprint(jac_eval_current_box)
+                   input()
                    if d.invertibility_of_a_matrix(jac_eval_current_box)==1:
                     Image_of_current_box=d.hansen_hengupta(mid_box,jac_eval_current_box,b,current_box,current_box)
-                    #ftprint(Image_of_current_box)
-                    #input()
                     if Image_of_current_box !='empty':
-                                    """ftprint(current_box)
-                                    ftprint(Image_of_current_box)
-                                    input()"""
                                     currecnt_box_contains_its_image=1
                                     for i  in range(len(Image_of_current_box)):
                                           if  (Image_of_current_box[i]).lower() <= (current_box[i]).lower() or (Image_of_current_box[i]).upper() >= (current_box[i]).upper():
